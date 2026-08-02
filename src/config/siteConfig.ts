891cfe33ec72bbe4c46287f4fa3904e1ad7a8603
Fly-Linn <487299732@qq.com>
2026-08-02 14:08:32 +0800

    学习创建第一个博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "风灵",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "灵山之上神风起",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
